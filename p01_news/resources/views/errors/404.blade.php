993bb4de9a684b243c0497939149fc5f31d8dda7
pstaleal <thanhhai_1993@yahoo.com>
2020-10-06 19:16:39 +0700

    error 404

--- p01_news/resources/views/errors/404.blade.php
+++ p01_news/resources/views/errors/404.blade.php
@@ -1,23 +1,29 @@
-@extends('errors.main')
+@extends('news.main',['title' => 'Error 404'])
 
 @section('content')
+<head>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Arvo'>
+  <link rel="stylesheet" href="{{ asset('error404/style.css') }}">
+
+</head>
   <section class="page_404">
     <div class="container">
-      <div class="row">
-          <div class="col-sm-12 ">
-            <div class="col-sm-10 col-sm-offset-1  text-center">
-                <div class="four_zero_four_bg">
-                  <h1 class="text-center ">404</h1>
-                </div>
-                <div class="contant_box_404">
-                  <h3 class="h2">
-                      Look like you're lost
-                  </h3>
-                  <p>the page you are looking for not avaible!</p>
-                  <a href="{{route('home')}}" class="link_404">Go to Home</a>
-                </div>
-            </div>
-          </div>
+      <div class="row">	
+      <div class="col-sm-12 ">
+      <div class="col-sm-10 col-sm-offset-1  text-center">
+      <div class="four_zero_four_bg">
+        <h1 class="text-center ">404</h1>
+      </div>
+      <div class="contant_box_404">
+      <h3 class="h2">
+      Look like you're lost
+      </h3>
+      <p>the page you are looking for not avaible!</p>
+      <a href="https://instagram.com/abol.codes" class="link_404">Go to Home</a>
+    </div>
+      </div>
+      </div>
       </div>
     </div>
 </section>
