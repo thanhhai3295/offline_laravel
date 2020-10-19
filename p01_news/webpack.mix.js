899734ed0bf8101e8dc93fa106c6e80eaf23607e
pstaleal <thanhhai_1993@yahoo.com>
2020-10-19 18:51:14 +0700

    webpack laravel mix

--- p01_news/webpack.mix.js
+++ p01_news/webpack.mix.js
@@ -10,8 +10,11 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.js('public/assmin/js/my-js.js', 'public/backend/js');
+mix.postCss('public/assmin/css/mycss.css', 'public/backend/css');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.combine([
+  'public/assmin/css/mycss.css',
+  'public/assmin/css/custom.min.css',
+  'public/assmin/css/toastr.css'
+],'public/backend/css/combine-all.css');
