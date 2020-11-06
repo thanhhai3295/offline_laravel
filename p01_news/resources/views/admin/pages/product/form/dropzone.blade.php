fba23df21447bb0368488f907cd01f2a0979f169
pstaleal <thanhhai_1993@yahoo.com>
2020-11-06 20:16:18 +0700

    edit dropzone

--- p01_news/resources/views/admin/pages/product/form/dropzone.blade.php
+++ p01_news/resources/views/admin/pages/product/form/dropzone.blade.php
@@ -8,11 +8,38 @@
       </div>
       <br>
       <div id="uploaded-image">
+        @php
+            use App\Helpers\Template as Template; 
+            if((isset($item['id']))) {
+              $thumb = json_decode($item['thumb'],true);
+              $xhtml = '';
+              foreach ($thumb as $key => $value) {
+                $id = trim(str_replace('.','-',$value['name']));
+                $input = '<input type="hidden" name="thumb[]" value="'.$value['name'].'">';
+                $alt = '<input type="text" placeholder="Alt Picture" name="alt[]" class="form-control" value="'.$value['alt'].'">';
+                $close = '<a href="javascript:void(0)" class="close-btn"><i class="fa fa-times"></i></a>';
+                $image = Template::showItemThumb($controllerName,$value['name'],$value['alt']);
+                $xhtml .= '<div class="col-md-3 cursor-pointer id="'.$id.'">'.$input.$image.$alt.$close.'</div>';
+              }
+              echo $xhtml;
+            }
+        @endphp
 
       </div>
     </div>
   </div>
 
+  @if (isset($item['id']))
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+    $('#uploaded-image').sortable();
+    $('#uploaded-image').disableSelection();
+    $('.close-btn').click(function(e) {
+     $(this).parent().remove();
+    });
+    </script>
+  @endif
   
 
 
